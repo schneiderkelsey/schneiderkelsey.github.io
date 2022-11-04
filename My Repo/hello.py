cecf77431b45a63e5ddaece89d493d16e5f92b5d
schneiderkelsey <77588740+schneiderkelsey@users.noreply.github.com>
2022-11-04 18:58:47 -0400

    python work added

---
+++ My Repo/hello.py
@@ -0,0 +1,21 @@
+name = "Paul"
+# a string type variable
+
+louder_name = name.upper()
+#using a string method
+
+def hello(name) :
+#a function that says hi
+    print("Hello " + name)
+
+hello(name)
+#calling the function, similar to using print
+
+>> "Hello Paul"
+#the output
+
+hello(louder_name)
+#calling the function again
+
+>> "Hello PAUL" 
+
