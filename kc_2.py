18085dd6f1840a435f94f1b444ff64fa1fd53452
schneiderkelsey <77588740+schneiderkelsey@users.noreply.github.com>
2022-11-08 19:50:09 -0500

    Fixed

---
+++ kc_2.py
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+dataframe = pd.read_csv("assets/deadbees.csv")
+
+dead = dataframe["dead"]
+dead = pd.to_datetime(dataframe["Dead_Year"], errors = "coerce")
+
+
+plt.plot(dead, "Dead_Year", marker="^")
+=======
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+dataframe = pd.read_csv("assets/deadbees.csv")
+
+dead = dataframe["dead"]
+dead = pd.to_datetime(dataframe["Dead_Year"], errors = "coerce")
+
+
+plt.plot(dead, "Dead_Year", marker="^")
+>>>>>>> 5a6d56690cdb8dc8797c6ea663ab86e8e4b86261
+plt.show()
